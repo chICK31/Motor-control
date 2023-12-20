0f58b34928de1a23f98f14bb0618520c27ae52fa
chICK31 <129692316+chICK31@users.noreply.github.com>
2023-12-19 19:30:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 function sendCommand(command) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', command, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            // Update the status after fetching the data
-            updateStatus(data.servoStatus);
-        }
-    };
     xhr.send();
-}
-
-function updateStatus(status) {
-    var statusElem = document.getElementById('status');
-    statusElem.textContent = 'Servo Status: ' + status;
 }

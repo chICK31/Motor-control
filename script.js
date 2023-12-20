42a16fed538bb2022c7a56fe2b0bbd0ebd4f8f3c
chICK31 <129692316+chICK31@users.noreply.github.com>
2023-12-19 19:24:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sendCommand(command) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', command + '.json', true); // Use command as the JSON file name
+    xhr.open('GET', command, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var data = JSON.parse(xhr.responseText);
